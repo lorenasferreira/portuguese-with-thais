87cb4ee7448d7654f0ce42b6d4022448ef2130a2
Lorena Ferreira <lollaaz@icloud.com>
2026-08-03 17:06:18 +0200

    chore: initialize Portuguese with Thais project

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <main className={styles.home}>
+      <h1>Portuguese with Thaís</h1>
+    </main>
+  );
+}
+
+export default Home;
